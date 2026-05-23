8f94417e64056df324a6410a1f57937f8cebea25
PyuraMazo <1605025385@qq.com>
2026-05-23 21:40:06 +0800

    feat:新增各个指令的英文别名

--- main.py
+++ main.py
@@ -40,19 +40,19 @@ class GalgameBoxPlugin(Star):
         """Galgame百宝盒插件的主指令"""
         pass
 
-    @gal_box.command("作品", alias={"游戏"})
+    @gal_box.command("作品", alias={"游戏","vn"})
     async def vn(self, event: AstrMessageEvent, keyword: str):
         """通过关键词查询作品"""
         async for res in self._common_command(event, CommandType.VN, keyword):
             yield res
 
-    @gal_box.command("角色", alias={"人物"})
+    @gal_box.command("角色", alias={"人物","character"})
     async def character(self, event: AstrMessageEvent, keyword: str):
         """通过关键词查询角色"""
         async for res in self._common_command(event, CommandType.CHARACTER, keyword):
             yield res
 
-    @gal_box.command("厂商", alias={"作者"})
+    @gal_box.command("厂商", alias={"作者","producer"})
     async def producer(self, event: AstrMessageEvent, keyword: str):
         """通过关键词查询厂商"""
         async for res in self._common_command(event, CommandType.PRODUCER, keyword):
@@ -64,43 +64,43 @@ class GalgameBoxPlugin(Star):
         async for res in self._common_command(event, CommandType.ID, keyword):
             yield res
 
-    @gal_box.command("简讯", alias={"游讯"})
+    @gal_box.command("简讯", alias={"event"})
     async def gal_event(self, event: AstrMessageEvent):
         """了解今日旮旯讯息，包括今天发售游戏与生日角色"""
         async for res in self._common_command(event, CommandType.EVENT):
             yield res
 
-    @gal_box.command("随机")
+    @gal_box.command("随机", alias={"random"})
     async def random(self, event: AstrMessageEvent):
         """通过TouchGal随机获取一部作品"""
         async for res in self._common_command(event, CommandType.RANDOM):
             yield res
 
-    @gal_box.command("下载", alias={"资源"})
+    @gal_box.command("下载", alias={"资源", "download"})
     async def download(self, event: AstrMessageEvent, id: str):
         """优先通过VNDB ID、TouchGal ID，最后通过关键词搜索获取指定资源的下载链接"""
         async for res in self._common_command(event, CommandType.DOWNLOAD, id):
             yield res
 
-    @gal_box.command("出处", alias={"识别"})
-    async def find(self, event: AstrMessageEvent, url: str = ""):
-        """提供图片或者图片链接识别角色出处，可以先不填参数"""
-        async for res in self._common_command(event, CommandType.FIND, url):
-            yield res
-
-    @gal_box.command("推荐", alias={"标签"})
+    @gal_box.command("推荐", alias={"标签", "recommend"})
     async def recommend(self, event: AstrMessageEvent, tags: GreedyStr):
         """提供一个或多个标签，从TouchGal网站中获取推荐内容"""
         async for res in self._common_command(event, CommandType.RECOMMEND, tags):
             yield res
 
-    @gal_box.command("绑定", alias={})
+    @gal_box.command("出处", alias={"识别", "find"})
+    async def find(self, event: AstrMessageEvent, url: str = ""):
+        """提供图片或者图片链接识别角色出处，可以先不填参数"""
+        async for res in self._common_command(event, CommandType.FIND, url):
+            yield res
+
+    @gal_box.command("绑定", alias={"bind"})
     async def bind(self, event: AstrMessageEvent):
         """通过Steam API和Steam ID绑定Steam账号，只能在私聊频道中生效"""
         async for res in self._common_command(event, CommandType.BIND):
             yield res
 
-    @gal_box.command("拼图", alias={"steam"})
+    @gal_box.command("拼图", alias={"puzzle"})
     async def puzzle(self, event: AstrMessageEvent):
         """生成基于Steam的游玩记录的Galgame总览"""
         async for res in self._common_command(event, CommandType.PUZZLE):
